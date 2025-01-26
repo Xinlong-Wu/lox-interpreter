75663803ab85629e5f8472561e2a13c338295e92
XinlongWu <43398706+Xinlong-Wu@users.noreply.github.com>
2025-01-26 09:58:21 +0000

    string + number

--- JLox/JLox/Interpreter/Interpreter.java
+++ JLox/JLox/Interpreter/Interpreter.java
@@ -65,13 +65,25 @@ public class Interpreter implements Expr.Visitor<Object> {
                 checkNumberOperands(expr.operator, left, right);
                 return (double) left * (double) right;
             case PLUS:
-                if (left instanceof Double && right instanceof Double) {
-                    return (double) left + (double) right;
+                if (left instanceof Double) {
+                    if (right instanceof String) {
+                        return stringify(left) + (String) right;
+                    }
+                    else if (right instanceof Double) {
+                        return (double) left + (double) right;
+                    }
                 }
 
-                if (left instanceof String && right instanceof String) {
-                    return (String) left + (String) right;
+                if (left instanceof String) {
+                    if (right instanceof Double) {
+                        return (String) left + stringify(right);
+                    }
+                    else if (right instanceof String) {
+                        return (String) left + (String) right;
+                    }
                 }
+
+
                 throw new RuntimeError(expr.operator,
                         "Operands must be two numbers or two strings.");
             case GREATER:
