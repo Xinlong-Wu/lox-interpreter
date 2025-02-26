70f1f55aea5d572907a20b009e60b7cb6d4f4de6
XinlongWu <43398706+Xinlong-Wu@users.noreply.github.com>
2025-02-26 09:09:34 +0000

    Local Var

--- CLox/src/vm/vm.c
+++ CLox/src/vm/vm.c
@@ -128,6 +128,16 @@ static InterpretResult run()
         case OP_TRUE: push(BOOL_VAL(true)); break;
         case OP_FALSE: push(BOOL_VAL(false)); break;
         case OP_POP: pop(); break;
+        case OP_GET_LOCAL: {
+            uint8_t slot = READ_BYTE();
+            push(vm.stack[slot]);
+            break;
+        }
+        case OP_SET_LOCAL: {
+            uint8_t slot = READ_BYTE();
+            vm.stack[slot] = peek(0);
+            break;
+        }
         case OP_GET_GLOBAL:
         case OP_GET_GLOBAL_LONG:{
             int len = instruction == OP_GET_GLOBAL ? 1 : 3;
